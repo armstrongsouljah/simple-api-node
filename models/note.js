60f49cf7de161fcd2090ec4f1bb5ec0313a716f4
Muhwezi Armstrong <armstrongsouljah@gmail.com>
2019-08-09 18:59:17 +0300

    feat (notes) : Create a note
    - Implement a note model
    - Implement save method
    - Add note saving controller
    - Wire routing to main app

--- models/note.js
+++ models/note.js
@@ -1,11 +1,21 @@
-const db = require('../database/index').getDB
+const getDB = require('../database/index').getDB
 class Note {
     constructor(title, body) {
         this.title = title
         this.body = body
     }
     saveNote() {
-        // save note code here
+        let db = getDB()
+        if(!db.collection('items')){
+            db.createCollection('items')
+        }
+        db.collection('items').insertOne(this)
+         .then(result => {
+             console.log(result)
+         })
+         .catch(error => {
+             throw error
+         })
     }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
 const router = require('express').Router()
+const noteController  = require('../controllers/noteControllers')
+router.post('/note', noteController.createNote)
 
 module.exports = router;
